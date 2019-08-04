55be0e9edf25f52bb389cfc48b8945302845dd90
mikebolin <mikebolin@gmail.com>
2019-08-04 15:10:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wlqwem
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wlqwem)
---
+++ src/app/app-module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from './shared/shared.module';
+
+import { CoreModule } from './core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+
+@NgModule({
+  imports: [
+    // angular
+    BrowserAnimationsModule,
+    BrowserModule,
+
+    // core & shared
+    CoreModule,
+    SharedModule,
+
+    // app
+    AppRoutingModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
