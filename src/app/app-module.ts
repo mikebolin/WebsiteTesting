4836d9daca6c5f8f86ddc9b8ca9e5c93dace19bd
mikebolin <mikebolin@gmail.com>
2019-08-04 17:39:42 -0400

    1 file modified

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,25 +3,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './shared/shared.module';
-
-import { CoreModule } from './core/core.module';
-
+import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   imports: [
-    // angular
     BrowserAnimationsModule,
     BrowserModule,
-
-    // core & shared
-    CoreModule,
     SharedModule,
-
-    // app
+    CoreModule,
     AppRoutingModule
   ],
   declarations: [AppComponent],
