047e1b94ae298b4476cf9e3a3f018a1193d03fa3
mikebolin <mikebolin@gmail.com>
2019-11-28 21:03:08 -0500

    1 file modified

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -9,15 +9,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 
 
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { GridModule, ExcelModule } from '@progress/kendo-angular-grid';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { DialogModule, WindowModule } from '@progress/kendo-angular-dialog';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { UploadModule } from '@progress/kendo-angular-upload';
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { ContextMenuModule } from '@progress/kendo-angular-menu';
+import { RippleModule } from '@progress/kendo-angular-ripple';
+import { DatePickerModule } from '@progress/kendo-angular-dateinputs';
+
+import { MultiCheckFilterComponent } from './featuresabout/components/multicheck-filter.component';
+
+import { SearchAutoComponent } from '.features/about/autocomplete/app-autocomplete';
+
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     SharedModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+        LayoutModule,
+    DateInputsModule,
+    GridModule,
+    DropDownsModule,
+    DialogModule,
+    WindowModule,
+    InputsModule,
+    UploadModule,
+    TreeViewModule,
+    ButtonsModule,
+    PopupModule,
+    ContextMenuModule,
+    ExcelModule,
+    RippleModule,
+    DatePickerModule
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, InvoiceGridGridComponent,  MultiCheckFilterComponent, SearchAutoComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/features/about/about.module.ts
+++ src/app/features/about/about.module.ts
@@ -12,42 +12,12 @@ import {MatAutocompleteModule, MatInputModule} from '@angular/material';
 import { InvoiceGridGridComponent } from '../about/components/invoice-grid.component';
 
 
-import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
-import { GridModule, ExcelModule } from '@progress/kendo-angular-grid';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { DialogModule, WindowModule } from '@progress/kendo-angular-dialog';
-import { InputsModule } from '@progress/kendo-angular-inputs';
-import { UploadModule } from '@progress/kendo-angular-upload';
-import { TreeViewModule } from '@progress/kendo-angular-treeview';
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { LayoutModule } from '@progress/kendo-angular-layout';
-import { PopupModule } from '@progress/kendo-angular-popup';
-import { ContextMenuModule } from '@progress/kendo-angular-menu';
-import { RippleModule } from '@progress/kendo-angular-ripple';
-import { DatePickerModule } from '@progress/kendo-angular-dateinputs';
 
-import { MultiCheckFilterComponent } from '../about/components/multicheck-filter.component';
-
-import { SearchAutoComponent } from '../about/autocomplete/app-autocomplete';
 @NgModule({
-  declarations: [AboutComponent, InvoiceGridGridComponent,  MultiCheckFilterComponent, SearchAutoComponent],
-  imports: [CommonModule, SharedModule, AboutRoutingModule, MatAutocompleteModule, MatInputModule,
+  declarations: [AboutComponent],
+  imports: [CommonModule, SharedModule, AboutRoutingModule, MatAutocompleteModule, MatInputModule
   
-    LayoutModule,
-    DateInputsModule,
-    GridModule,
-    DropDownsModule,
-    DialogModule,
-    WindowModule,
-    InputsModule,
-    UploadModule,
-    TreeViewModule,
-    ButtonsModule,
-    PopupModule,
-    ContextMenuModule,
-    ExcelModule,
-    RippleModule,
-    DatePickerModule],
+],
   entryComponents: [],
   bootstrap: [],
 })
